cae9cfdf3546e2558a8a3f05f0a34cc440ade50b
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-23 21:17:38 -0500

    added get random answer method

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,10 @@ const URL_PREFIX = "https://pictionar-eh-api-7e9c6522d932.herokuapp.com"
 const API = {
     getAllAnswers:() => {
         return fetch(`${URL_PREFIX}/api/answers`).then(res=>res.json())
+    },
+
+    getRandomAnswer:() => {
+        return fetch(`${URL_PREFIX}/api/answers/random`).then(res=>res.json())
     }
 }
 
