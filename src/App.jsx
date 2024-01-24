2161f7716800fb31d9b256f5fae4d935cbe8bb01
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-23 20:44:31 -0500

    created functioning router for home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import Homepage from "./pages/home"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './pages/home/index.jsx';
+import Game from './pages/game'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Error />,
+    // errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Home />,
+        element: <Homepage />,
       },
       {
-        path: '/About',
-        element: <About />,
+        path: '/game',
+        element: <Game />,
       },
-      {
-        path: '/Blog',
-        element: <Blog />,
-      },
-      {
-        path: '/Contact',
-        element: <Contact />,
-      },
+      // {
+      //   path: '/Blog',
+      //   element: <Blog />,
+      // },
+      // {
+      //   path: '/Contact',
+      //   element: <Contact />,
+      // },
     ],
   },
 ]);
