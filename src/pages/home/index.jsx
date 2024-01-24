bb02c10b61efebe758f8f878a0c5292af708841b
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-23 21:10:10 -0500

    tested api routes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./style.css"
+import API from "../../utils/API"
 
 function Homepage() {
+  useEffect(()=>{
+    API.getAllAnswers().then(answerData=>{
+      console.log(answerData);
+    }).catch(err=>{
+      console.log(err)
+    })
+  })
     return (
       <header className="header">
         <h1>Welcome to Pictionar-eh</h1>
