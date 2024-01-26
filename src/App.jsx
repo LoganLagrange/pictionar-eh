8dad1156d674e169cc65d25908094926886c685e
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-25 19:26:59 -0500

    added imports and files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./pages/home/index";
 import JoinRoom from './components/RoomForm';
+import API from "./utils.API";
+import { useState } from "react";
+import authForm from "./pages/authForm"
 // import CreateRoom from './components/RoomForm';
 
 function App() {
