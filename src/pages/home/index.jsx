e7ecf421005f8e4a70eef5ce434c92f53e7130f8
Thom Wollin <thomwollin@gmail.com>
2024-01-25 19:10:03 -0500

    b

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import "./style.css"
 import API from "../../utils/API"
