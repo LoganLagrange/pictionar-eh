85fc3c71ccee9789b67437926ae286f524b31611
Thom Wollin <thomwollin@gmail.com>
2024-01-25 20:59:41 -0500

    added gameroom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./pages/home/index";
+import Gamepage from "./pages/gameroom/index";
 import JoinRoom from './components/RoomForm';
+
 // import CreateRoom from './components/RoomForm';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage/>} />
         <Route path="/join-room" element={<JoinRoom/>} />
+        <Route path="/gamepage" element={<Gamepage/>} />
         {/* <Route path="/create-room" component={CreateRoom} /> */}
       </Routes>
     </Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
