ecbacd46e584d0983664833773add7b8ab28a4dc
Logan Lagrange <lagrangelogan@gmail.com>
2024-02-01 18:55:34 -0500

    fixing

--- src/pages/authForm/index.jsx
+++ src/pages/authForm/index.jsx
@@ -3,21 +3,26 @@ import "./style.css";
 import API from "../../utils/API";
 
 export default function AuthForm(props) {
-  const [username, setUsername] = useState("")
+  const [username, setusername] = useState("")
   const [password, setPassword] = useState("")
-  const subHandle = e=>{
+  const subHandle = async (e)=>{
     e.preventDefault();
-    props.handleSubmit({
-      username,
-      password
-    })
+    try {
+      const response = await API.signup({
+        username,
+        password
+      });
+    console.log("Signup successful:", response);
+  }catch (error) {
+    console.log("Signup failed", error.message);
+  }
   }
  
   return (
     <div className="AuthForm">
       <h3>{props.type}</h3>
       <form onSubmit={subHandle}>
-        <input type="text" value={username} onChange={e=>setUsername(e.target.value)} />
+        <input type="text" value={username} onChange={e=>setusername(e.target.value)} />
         <input type="password" value={password} onChange={e=>setPassword(e.target.value)} />
         <button>{props.type}</button>
       </form>
