be7389e2156e0e3c61e9418b4444eb0cd22f73a6
Logan Lagrange <lagrangelogan@gmail.com>
2024-02-01 21:09:39 -0500

    removed unused code

--- src/components/GameRooms/RoomCard.jsx
+++ src/components/GameRooms/RoomCard.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import socketUse from '../../utils/socket';
 import { useNavigate } from 'react-router-dom';
 
-const handleClick = (roomName, navigate) =>{
-  console.log(roomName);
-  socketUse.joinRoom(roomName); // supposed to join the room on the socket
-  setCurrentRoom()
-  navigate('/game')
-}
-
-const RoomCard = ({ room }) => {
+const RoomCard = ({ room, currentRoom, setRoom }) => {
   const navigate = useNavigate();
+
+  const handleClick = (roomName, navigate) =>{
+    console.log(roomName);
+    socketUse.joinRoom(roomName); // supposed to join the room on the socket
+    setRoom(roomName)
+    navigate('/game')
+  }
+
   return (
     <div className="room-card" id={room.name}>
       <h3>{room.name}</h3> 
